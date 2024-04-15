5ab05b79b06ef00e9730b7476174908bc4be1f92
NikolayKochnev <163389484+NikolayKochnev@users.noreply.github.com>
2024-04-15 14:52:11 +0700

    Add files via upload

---
+++ homework14.py
@@ -0,0 +1,19 @@
+#Вызов функцию inner_function внутри функции test_function
+
+#def test_function():
+
+#   def inner_function():
+#        print('Я в области видимости функции test_function')
+#    inner_function()
+#test_function()
+
+
+#Вызов inner_function вне функции test_function
+
+#def test_function():
+
+#    def inner_function():
+#        print('Я в области видимости функции test_function')
+#inner_function()
+#test_function()
+
